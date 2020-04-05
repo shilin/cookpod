f8c70e74afa00a466c2c480854a63dc21ced70d3
Alexei Shilin <alexei.shilin@gmail.com>
2020-04-05 13:39:36 +0700

    Revert "break github action badge"
    This reverts commit 60c6b346073ca41e226eb2b13043b67239377529.

--- test/cookpod_web/controllers/page_controller_test.exs
+++ test/cookpod_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule CookpodWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to BADGE Error Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
 end
