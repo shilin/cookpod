4aab21f029b2d8d1e8f6b789fc7c741566c1d7bd
Alexei Shilin <alexei.shilin@gmail.com>
2020-04-11 11:46:49 +0700

    add ActionClauseError  handling as 422

--- lib/cookpod_web/router.ex
+++ lib/cookpod_web/router.ex
@@ -6,7 +6,7 @@ defmodule CookpodWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-    plug :protect_from_forgery
+    # plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
 
@@ -39,6 +39,18 @@ defmodule CookpodWeb.Router do
     |> put_view(CookpodWeb.ErrorView)
     |> render("404.html")
   end
+
+  def handle_errors(conn, %{kind: :error, reason: %Phoenix.ActionClauseError{}}) do
+    conn
+    |> fetch_session()
+    |> fetch_flash()
+    |> put_layout({CookpodWeb.LayoutView, :app})
+    |> put_view(CookpodWeb.ErrorView)
+    |> render("422.html")
+  end
+
+  def handle_errors(conn, _), do: conn
+
 
   # Other scopes may use custom stacks.
   # scope "/api", CookpodWeb do
--- test/cookpod_web/views/error_view_test.exs
+++ test/cookpod_web/views/error_view_test.exs
@@ -8,6 +8,10 @@ defmodule CookpodWeb.ErrorViewTest do
     assert render_to_string(CookpodWeb.ErrorView, "404.html", []) =~ "Not Found"
   end
 
+  test "renders 422.html" do
+    assert render_to_string(CookpodWeb.ErrorView, "422.html", []) =~ "Unprocessable Entity"
+  end
+
   test "renders 500.html" do
     assert render_to_string(CookpodWeb.ErrorView, "500.html", []) == "Internal Server Error"
   end
