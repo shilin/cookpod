b4d706a3445890e350b5be4ad663e75b922a1589
Alexei Shilin <alexei.shilin@gmail.com>
2020-04-11 12:55:30 +0700

    bring back csrf protection

--- lib/cookpod_web/router.ex
+++ lib/cookpod_web/router.ex
@@ -6,7 +6,7 @@ defmodule CookpodWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-    # plug :protect_from_forgery
+    plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
 
