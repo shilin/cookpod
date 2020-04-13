2d8c7633280938a883052db847ab9a2e08d55f33
Alexei Shilin <alexei.shilin@gmail.com>
2020-04-13 18:27:48 +0700

    add test for session controller using init_test_session

--- test/cookpod_web/controllers/session_controller_test.exs
+++ test/cookpod_web/controllers/session_controller_test.exs
@@ -1,11 +1,21 @@
 defmodule CookpodWeb.SessionControllerTest do
   use CookpodWeb.ConnCase
+  import Plug.Test
+
   @username Application.get_env(:cookpod, :basic_auth)[:username]
   @password Application.get_env(:cookpod, :basic_auth)[:password]
 
   defp using_basic_auth(conn, username, password) do
     header_content = "Basic " <> Base.encode64("#{username}:#{password}")
     put_req_header(conn, "authorization", header_content)
+  end
+
+  test "GET /sessions as logged in user", %{conn: conn} do
+    conn = conn
+      |> using_basic_auth(@username, @password)
+      |> init_test_session(%{current_user: "test-user"})
+      |> get(Routes.session_path(conn, :show))
+    assert html_response(conn, 200) =~ "You are logged in"
   end
 
   test "GET /sessions", %{conn: conn} do
