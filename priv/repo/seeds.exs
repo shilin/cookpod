e57322b0a91828fa6c4fa81c15f325f876b18093
Alexei Shilin <alexei.shilin@gmail.com>
2020-04-16 00:43:04 +0700

    populate users from seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,15 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Cookpod.Repo
+alias Cookpod.User
+
+users = [
+  %{email: "user@test.com", password: "1111", password_confirmation: "1111"},
+  %{email: "admin@test.com", password: "1234", password_confirmation: "1234"}
+]
+
+Enum.each(users, fn user ->
+  Repo.insert!(User.changeset(%User{}, user))
+end)
