336f67dab0ca786286a27d2d168e9e540fb98d69
Alexei Shilin <alexei.shilin@gmail.com>
2020-06-16 15:09:59 +0700

    add batch processing to kafka recipes

--- lib/cookpod/recipes/Loot.ex
+++ lib/cookpod/recipes/Loot.ex
@@ -4,6 +4,7 @@ defmodule Cookpod.Recipes.Loot do
   # import Ecto.Changeset
   alias Cookpod.Recipes.Recipe
   alias Cookpod.Repo
+  alias Broadway.Message
 
   def start_link(_opts \\ []) do
     Broadway.start_link(__MODULE__,
@@ -22,17 +23,42 @@ defmodule Cookpod.Recipes.Loot do
         default: [
           concurrency: 10
         ]
+      ],
+      batchers: [
+        default: [
+          concurrency: 5,
+          batch_size: 10,
+          batch_timeout: 1000
+        ]
       ]
     )
   end
 
-  def handle_message(_, message, _) do
-    %{"name" => name, "description" => description} = Jason.decode!(message.data)
-    # IO.inspect(Jason.decode!(message.data), label: "Got message")
-    %Recipe{}
-    |> Recipe.changeset(%{name: name, description: description})
-    |> Repo.insert(on_conflict: :replace_all, conflict_target: :name)
+  # def handle_message(_, message, _) do
+  #   %{"name" => name, "description" => description} = Jason.decode!(message.data)
+  #   # IO.inspect(Jason.decode!(message.data), label: "Got message")
+  #   %Recipe{}
+  #   |> Recipe.changeset(%{name: name, description: description})
+  #   |> Repo.insert(on_conflict: :replace_all, conflict_target: :name)
 
+  #   message
+  # end
+
+  def handle_message(_processor_name, message, _context) do
     message
+    |> Message.update_data(fn data ->
+      %{"name" => name, "description" => description} = Jason.decode!(data)
+      %Recipe{} |> Recipe.changeset(%{name: name, description: description})
+    end)
+  end
+
+  def handle_batch(_, messages, _batch_info, _context) do
+    messages
+    |> Enum.each(fn %{data: data} ->
+      if data.valid? do
+        Repo.insert(data, on_conflict: :replace_all, conflict_target: :name)
+      end
+    end)
+    messages
   end
 end
